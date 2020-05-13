c9addf7bbb89b954db77686eb7ea87c4add84e16
tznind <t.z.nind@dundee.ac.uk>
2020-05-13 19:25:00 +0100

    Followed GettingStarted tutorial

---
+++ Game/Program.cs
@@ -0,0 +1,20 @@
+﻿using Terminal.Gui;
+using Wanderer.Factories;
+using Wanderer.TerminalGui;
+
+namespace TestWandererInstructions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var worldFactory = new WorldFactory();
+            
+            Application.Init();
+                            
+            var ui = new MainWindow(worldFactory);
+            Application.Top.Add(ui);
+            Application.Run();             
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # WandererTemplate
-Template for the Wanderer engine
+Template for the [Wanderer engine](https://github.com/tznind/Wanderer)
+
+This was produced by following the 'Getting Started' instructions
